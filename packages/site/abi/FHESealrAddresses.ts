2a15dad4f8f7875afeba7cdd9da11a0678ba4dbe
nodelife88 <quoctuannso@gmail.com>
2025-10-27 11:02:50 +0700

    update new contract

--- packages/site/abi/FHESealrAddresses.ts
+++ packages/site/abi/FHESealrAddresses.ts
@@ -6,7 +6,7 @@ export type SealrChainInfo = {
 
 export const FHESealrAddresses: Record<string, SealrChainInfo> = {
   "11155111": {
-    address: "0x76dd5b0881944B39206230227ebcf0E00dd425b7",
+    address: "0x841218717Cd49278C86Cd3931e1c0D28703e545D",
     chainId: 11155111,
     chainName: "sepolia",
   }
